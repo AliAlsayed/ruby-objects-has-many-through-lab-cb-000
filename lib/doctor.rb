d0b1e15ae3492c8baf9536246ca6a7f74612b383
Ali Alsayed <a_alsayed87@hotmail.com>
2018-11-05 14:32:14 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,6 +11,7 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-    appointment = Appointment.new(patient, self, date)
+    appointment = Appointment.new(self, patient, date)
   end
+
 end
