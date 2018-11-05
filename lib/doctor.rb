1a83d8100555a34bd60a370c6c2ac57efec3cccd
Ali Alsayed <a_alsayed87@hotmail.com>
2018-11-05 14:28:17 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,10 @@
 class Doctor
-  attr_accessor :name
+  attr_accessor :name, :appointments
   @@all = []
   def initialize(name)
     @name = name
     @@all << self
+    @appointments = []
   end
 
   def self.all
@@ -12,5 +13,6 @@ class Doctor
 
   def new_appointment(patient, date)
     appointment = Appointment.new(patient, self, date)
+    @appointments << appointment
   end
 end
